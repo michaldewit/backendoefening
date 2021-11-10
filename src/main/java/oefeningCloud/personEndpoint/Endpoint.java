065831ded7af86ea6c889141316d382cb39e67ac
michaldewit <michaldewit@gmail.com>
2021-11-10 16:01:10 +0100

    tweede commit

--- src/main/java/oefeningCloud/personEndpoint/Endpoint.java
+++ src/main/java/oefeningCloud/personEndpoint/Endpoint.java
@@ -1,13 +1,15 @@
 package oefeningCloud.personEndpoint;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(maxAge=3600)
 public class Endpoint {
 
-	@GetMapping("zozo/")
+	@GetMapping("zozo")
 	public Werknemer2 daarGaanwe() {
 		System.out.println("hij doet het ");
 		Werknemer2 w = new Werknemer2();
